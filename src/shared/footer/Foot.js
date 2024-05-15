869079269130601cd7e6aab39b9fe1036aa9d20b
pironoid14 <peteremieje@gmail.com>
2024-05-15 12:31:59 +0100

    folders created and linked

---
+++ src/shared/footer/Foot.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './foot.css'
+const Foot = () => {
+  return (
+    <div className="body">Foot</div>
+  )
+}
+
+export default Foot
---
+++ src/shared/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './header.css';
+const Header = () => {
+  return (
+    <div className="header-main">header</div>
+  )
+}
+
+export default Header;
